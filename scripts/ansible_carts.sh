c899f088c707b838fbad65021e8e8696f3fa4f11
Isakova Svitlana <Isakova Svitlana>
2019-11-22 15:47:01 +0000

    test

--- scripts/ansible_carts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#cd /opt/git/Learning_Ansible_containers_vsTerraform/Ansible/
-ansible-playbook carts_playbook/Ansible/carts_db.yaml
--- scripts/ansible_frontend.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ansible-playbook frontend/Ansible/front-end.yaml
